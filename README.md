2cf5f6e4602cab051543c6062d3a4565f18232f6
yeohj0710 <yeohj0710@yonsei.ac.kr>
2023-02-05 16:49:29 +0900

    9.4

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # app-practice web
+
+- [ ] Router
+- [ ] Authentication
+- [ ] Architecture (components postition)
+- [ ] Styles (ex : dark mode)
--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./screens/Home";
+import Login from "./screens/Login";
+import NotFound from "./screens/NotFound";
+
 function App() {
-  return <div></div>;
+  const isLoggedIn = false;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          {isLoggedIn ? <Home /> : <Login />}
+        </Route>
+        <Route>
+          <NotFound />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
