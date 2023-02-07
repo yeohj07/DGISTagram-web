0c56951258bc4b8871fda97bb09187e95ecbe9fa
yeohj0710 <yeohj0710@yonsei.ac.kr>
2023-02-07 22:49:12 +0900

    11.14

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { ApolloProvider, useReactiveVar } from "@apollo/client";
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./screens/Home";
 import Login from "./screens/Login";
 import NotFound from "./screens/NotFound";
--- src/apollo.js
+++ src/apollo.js
@@ -1,6 +1,7 @@
 import { ApolloClient, InMemoryCache, makeVar } from "@apollo/client";
 
 const TOKEN = "token";
+const DARK_MODE = "DARK_MODE";
 
 export const isLoggedInVar = makeVar(Boolean(localStorage.getItem(TOKEN)));
 
@@ -11,10 +12,22 @@ export const logUserIn = (token) => {
 
 export const logUserOut = () => {
   localStorage.removeItem(TOKEN);
-  isLoggedInVar(false);
+  window.location.reload();
 };
 
-export const darkModeVar = makeVar(false);
+export const darkModeVar = makeVar(
+  Boolean(localStorage.getItem(DARK_MODE) === "enabled")
+);
+
+export const enableDarkMode = () => {
+  localStorage.setItem(DARK_MODE, "enabled");
+  darkModeVar(true);
+};
+
+export const disableDarkMode = () => {
+  localStorage.removeItem(DARK_MODE, "enabled");
+  darkModeVar(false);
+};
 
 export const client = new ApolloClient({
   uri: "http://localhost:4000/graphql",
