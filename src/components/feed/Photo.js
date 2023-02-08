af80308391ecdd7341edb3bcf9c7ee4467829223
yeohj0710 <yeohj0710@yonsei.ac.kr>
2023-02-08 13:40:19 +0900

    12.10

--- src/components/feed/Photo.js
+++ src/components/feed/Photo.js
@@ -80,19 +80,28 @@ function Photo({ id, user, file, isLiked, likes }) {
       },
     } = result;
     if (ok) {
-      cache.writeFragment({
-        id: `Photo:${id}`,
-        fragment: gql`
-          fragment BSName on Photo {
-            isLiked
-            likes
-          }
-        `,
-        data: {
-          isLiked: !isLiked,
-          likes: isLiked ? likes - 1 : likes + 1,
-        },
+      const fragmentId = `Photo:${id}`;
+      const fragment = gql`
+        fragment BSName on Photo {
+          isLiked
+          likes
+        }
+      `;
+      const result = cache.readFragment({
+        id: fragmentId,
+        fragment,
       });
+      if ("isLiked" in result && "likes" in result) {
+        const { isLiked: cacheIsLiked, likes: cacheLikes } = result;
+        cache.writeFragment({
+          id: fragmentId,
+          fragment,
+          data: {
+            isLiked: !cacheIsLiked,
+            likes: cacheLikes ? cacheLikes - 1 : cacheLikes + 1,
+          },
+        });
+      }
     }
   };
   const [toggleLikeMutation, { loading }] = useMutation(TOGGLE_LIKE_MUTATION, {
